9461a5353e991e2604b2e55f821af51be950a02d
KyungminChoiGIST <kyungmin.choi@gistory.me>
2020-07-24 17:24:00 +0900

    Create print.py

---
+++ pythonStudy/print.py
@@ -0,0 +1,9 @@
+print('Hello World!')
+print(r'c\asdf\asdf') #r means raw string. it ignores any syntax rules inside.
+print(
+"""sdf
+            HEy
+            see for yourself
+""")                  #""" means multiline.
+
+print('a',end=",")   # parameter registered at 'end' is put at the very end
