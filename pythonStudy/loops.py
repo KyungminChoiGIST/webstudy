16817ff0e9c92086c2b707e85dd1b1ca61649b7b
KyungminChoiGIST <kyungmin.choi@gistory.me>
2020-07-24 17:39:12 +0900

    Update loops.py

--- pythonStudy/loops.py
+++ pythonStudy/loops.py
@@ -10,6 +10,14 @@ while a<10:
 print(result) #prints 45
 """
 #for loop
+# 1. iteration through list
+"""
 a=['qwer','asdf','zxcv']
 for w in a: #works like for-each loop in java
-    print(w, end=",")
+    print(w, end=",") #prints qwer,asdf,zxcv,
+"""
+# 2. iteration through range()
+"""
+for a in range(5) :
+    print(a) #prints 0\n 1 \n 2\n 3\n 4\n
+"""
