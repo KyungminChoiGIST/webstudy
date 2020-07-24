c22273aa9ea668c3fe35a73d3e5104e7f1ca9cbb
KyungminChoiGIST <kyungmin.choi@gistory.me>
2020-07-24 17:23:57 +0900

    Create loops.py

---
+++ pythonStudy/loops.py
@@ -0,0 +1,15 @@
+
+#while loop
+"""
+a, result=1, 0
+
+while a<10:
+    result+=a # adds 1 to 9
+    a+=1 #iteration control
+
+print(result) #prints 45
+"""
+#for loop
+a=['qwer','asdf','zxcv']
+for w in a: #works like for-each loop in java
+    print(w, end=",")
