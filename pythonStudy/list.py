d7d05b022fdbdc14acab51c13b59198b27aa7c14
KyungminChoiGIST <kyungmin.choi@gistory.me>
2020-07-24 17:23:53 +0900

    Create list.py

---
+++ pythonStudy/list.py
@@ -0,0 +1,10 @@
+square = [10,20,30,40,50]
+print(square) #prints [10,20,30,40,50]
+print(square[2]) #prints 30, index counted from 0
+
+#<list>.append(<target>)
+square.append(60)
+print(square) #prints [10,20,30,40,50,60]
+
+#len()
+print(len(square)) #prints 6
