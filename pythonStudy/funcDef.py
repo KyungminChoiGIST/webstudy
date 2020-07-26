cd0230ffa85d26ddc34cb06013dcea799ce8f4da
KyungminChoiGIST <kyungmin.choi@gistory.me>
2020-07-26 22:49:23 +0900

    Create funcDef.py

---
+++ pythonStudy/funcDef.py
@@ -0,0 +1,10 @@
+
+#function definition example
+def fib(n) :
+    """print fibonacci sequence where all the numbers are all under n"""
+    a,b = 1,1
+    while a<n :
+        print(a, end=" ")
+        a, b = b, a+b
+
+fib(10)
